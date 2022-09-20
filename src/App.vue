954540aabbca8aed16ac5e7fd6eca932f1d3b0c0
Elias <souzabrancoelias8@gmail.com>
2022-09-20 09:24:09 -0300

    Elias

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      cep: "",
+      resultado: {},
+    };
+  },
+  methods: {
+    async buscar() {
+      const url = `https://viacep.com.br/ws/${this.cep}/json/`;
+      const { data } = await axios.get(url);
+      this.endereço = data;
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>Buscar Cep</h1>
+  <form @submit.prevent="buscar">
+    <input type="text" v-model="cep" placeholder="Digite o CEP" />
+    <button type="submit">Buscar</button>
+  </form>
+  <div 
+    <label for="rua">Rua</label>
+    <input name="rua" type="text" v-model="endereco.logradouro"/>
+    <p> Complemento: {{ endereco.complemento}}</p>
+    <p> Cidade: {{ endereco.localidade}}</p>
+    <p> Estado: {{ endereco.uf}}</p>
+  </div>
+</template>
